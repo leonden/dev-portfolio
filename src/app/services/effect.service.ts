cf595e66bf9930173ff7091bbdbbe22662fa1c5c
leonden <info@leonhochwimmer.com>
2023-10-21 22:56:46 +0200

    Create effect service

---
+++ src/app/services/effect.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EffectService {
+  anchorText: string = '';
+  anchorLetters: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+  constructor() {}
+
+  /**
+   *
+   * @param e The `MouseEvent` to to trigger the effect.
+   */
+  anchorHover(e: MouseEvent) {
+    const target = e.target as HTMLElement;
+    const anchor = target.closest('a');
+
+    if (anchor instanceof HTMLAnchorElement) {
+      const datasetValue = target.dataset['value'] || '';
+      const randomLetters = Array.from(
+        { length: anchor.innerHTML.length },
+        () => this.anchorLetters[Math.floor(Math.random() * 36)]
+      );
+
+      let iterations = 0;
+
+      const interval = setInterval(() => {
+        if (iterations < datasetValue.length) {
+          const resolved = anchor.innerHTML
+            .split('')
+            .map((letter, index) =>
+              index <= iterations ? datasetValue[index] : randomLetters[index]
+            )
+            .join('');
+
+          anchor.innerHTML = resolved;
+        }
+
+        if (iterations >= datasetValue.length) {
+          clearInterval(interval);
+        }
+
+        iterations += 1;
+      }, 40);
+    }
+  }
+}
