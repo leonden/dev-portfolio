601b9edea058b181b6739bf4e5a11fc7048fcdad
Leon <leon.hochwimmer@breitband.ch>
2023-12-20 20:49:00 +0100

    Start parallax slider

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,72 @@
+import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useMotionValue,
+  useVelocity,
+  useAnimationFrame,
+} from "framer-motion";
+import { wrap } from "@motionone/utils";
+
+interface ParallaxProps {
+  children: string;
+  baseVelocity: number;
+}
+
+function ParallaxText({ children, baseVelocity = 50 }: ParallaxProps) {
+  const baseX = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 80,
+    stiffness: 100,
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+    clamp: false,
+  });
+
+  const x = useTransform(baseX, (v) => `${wrap(-20, 20, v)}%`);
+
+  const directionFactor = useRef<number>(1);
+  useAnimationFrame((t, delta) => {
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -1;
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 1;
+    }
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+    baseX.set(baseX.get() + moveBy);
+  });
+
+  return (
+    <div className="overflow-hidden m-0 whitespace-no-wrap flex flex-no-wrap">
+      <motion.div
+        className="font-rota uppercase text-6xl flex whitespace-no-wrap flex-no-wrap"
+        style={{ x }}
+      >
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+      </motion.div>
+    </div>
+  );
+}
+
 function About() {
   return (
-    <>
-      <section id="about" className="w-screen h-screen">
-        <h1>About</h1>
-      </section>
-    </>
+    <section id="about" className="w-screen h-screen">
+      <ParallaxText baseVelocity={-2.5}>DEVELOPER</ParallaxText>
+      <ParallaxText baseVelocity={2.5}>PHOTOGRAPHER</ParallaxText>
+    </section>
   );
 }
 
