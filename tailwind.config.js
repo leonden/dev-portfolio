3b2dae3c2a76c485977a19f95e939e34761ac6a7
Leon <leon.hochwimmer@breitband.ch>
2023-12-20 18:47:55 +0100

    Add webfonts

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        rota: ["Rota", "sans-serif"],
+        mentra: ["Mentra", "sans-serif"],
+        symmetrika: ["Symmetrika", "sans-serif"],
+        tostada: ["Tostada", "serif"],
+      },
+    },
+    plugins: [],
+  },
+};
