0cc8efe86fd0ba617dd5cc69a48180313ec24471
Leon <leon.hochwimmer@breitband.ch>
2023-12-20 21:10:28 +0100

    Minor change

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import { motion, useScroll, useSpring } from "framer-motion";
 
 function App() {
@@ -13,13 +13,13 @@ function App() {
 
   return (
     <>
+      <Home />
+      <About />
+      <Contact />{" "}
       <motion.div
         className="translate-x-0 fixed bottom-0 left-0 right-0 h-1 bg-gradient-to-r from-indigo-300 to-purple-400 origin-left"
         style={{ scaleX }}
       />
-      <Home />
-      <About />
-      <Contact />
     </>
   );
 }
