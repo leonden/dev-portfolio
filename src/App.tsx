86e8131efc311927a380875e1aa075ee9074c28f
Leon <leon.hochwimmer@breitband.ch>
2023-12-23 11:26:56 +0100

    Adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import { motion, useScroll, useSpring } from "framer-motion";
+import ProgressBar from "./components/ProgressBar";
+import TextSlider from "./components/TextSlider";
 
 function App() {
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
-  });
-
   return (
     <>
       <Home />
+      <TextSlider />
       <About />
-      <Contact />{" "}
-      <motion.div
-        className="translate-x-0 fixed bottom-0 left-0 right-0 h-1 bg-gradient-to-r from-indigo-300 to-purple-400 origin-left"
-        style={{ scaleX }}
-      />
+      <Contact />
+      <ProgressBar />
     </>
   );
 }
