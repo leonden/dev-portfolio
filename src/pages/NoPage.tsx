4b26e6792e4ea6756d29f7d6ed1125276df3a5e0
Leon <leon.hochwimmer@breitband.ch>
2023-12-25 11:40:02 +0100

    Minor changes 404

--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,7 +1,12 @@
 function NoPage() {
   return (
     <>
-      <h1 className="font-mentra font-bold italic">404</h1>
+      <div id="no-page" className="w-screen h-screen grid place-items-center">
+        <div className="text-center">
+          <h1 className="font-mentra font-bold italic text-6xl">404</h1>
+          <h2 className="font-rota">The requested page doesn't exist :(</h2>
+        </div>
+      </div>
     </>
   );
 }
