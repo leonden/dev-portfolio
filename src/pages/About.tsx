65e2ed657beadad3b0b2b85f7429f748c9f2d297
Leon <leon.hochwimmer@breitband.ch>
2024-04-23 19:24:09 +0200

    Migrate to nextjs

--- src/pages/About.tsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <section id="about" className="w-screen h-screen z-10">
-      <h1>About</h1>
-    </section>
-  );
-}
--- src/pages/Contact.tsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-  return (
-    <>
-      <section id="contact" className="w-screen h-screen z-10">
-        <h1>Contact</h1>
-      </section>
-    </>
-  );
-}
