721dba0ea5da990ac429220d216c459d9f4d770b
hocl1 <leon.hochwimmer@coop.ch>
2024-04-24 14:44:31 +0200

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next"
 import "./globals.css"
 import React from "react"
 import content from "../content/en.json"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
     title: content.title,
@@ -16,6 +18,8 @@ export default function RootLayout({
     return (
         <html lang="en" className="scroll-smooth">
             <body className="font-rota">{children}</body>
+            <Analytics />
+            <SpeedInsights />
         </html>
     )
 }
