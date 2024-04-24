c54b3d98ff88ecd89f32ca44478b3499bbce377e
hocl1 <leon.hochwimmer@coop.ch>
2024-04-24 11:18:22 +0200

    Remove styling

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -15,27 +15,18 @@ export default function Navigation() {
                 <Menubar className="w-full justify-between p-0">
                     <MenubarMenu>
                         <MenubarTrigger>
-                            <Link href={"#home"} className="text-lg">
-                                Home
-                            </Link>
+                            <Link href={"#home"}>Home</Link>
                         </MenubarTrigger>
                     </MenubarMenu>
                     <MenubarMenu>
                         <MenubarTrigger className="lg:hidden">
-                            <Link href={"#about"} className="text-lg">
-                                About
-                            </Link>
+                            <Link href={"#about"}>About</Link>
                         </MenubarTrigger>
                         <MenubarTrigger className="hidden lg:block">
                             <TooltipProvider>
                                 <Tooltip>
                                     <TooltipTrigger asChild>
-                                        <Link
-                                            href={"#about"}
-                                            className="text-lg"
-                                        >
-                                            About
-                                        </Link>
+                                        <Link href={"#about"}>About</Link>
                                     </TooltipTrigger>
                                     <TooltipContent>
                                         <p>Who am I</p>
@@ -46,20 +37,13 @@ export default function Navigation() {
                     </MenubarMenu>
                     <MenubarMenu>
                         <MenubarTrigger className="lg:hidden">
-                            <Link href={"#work"} className="text-lg">
-                                Work
-                            </Link>
+                            <Link href={"#work"}>Work</Link>
                         </MenubarTrigger>
                         <MenubarTrigger className="hidden lg:block">
                             <TooltipProvider>
                                 <Tooltip>
                                     <TooltipTrigger asChild>
-                                        <Link
-                                            href={"#work"}
-                                            className="text-lg"
-                                        >
-                                            Work
-                                        </Link>
+                                        <Link href={"#work"}>Work</Link>
                                     </TooltipTrigger>
                                     <TooltipContent>
                                         <p>What do I do</p>
@@ -70,20 +54,13 @@ export default function Navigation() {
                     </MenubarMenu>
                     <MenubarMenu>
                         <MenubarTrigger className="lg:hidden">
-                            <Link href={"#contact"} className="text-lg">
-                                Contact
-                            </Link>
+                            <Link href={"#contact"}>Contact</Link>
                         </MenubarTrigger>
                         <MenubarTrigger className="hidden lg:block">
                             <TooltipProvider>
                                 <Tooltip>
                                     <TooltipTrigger asChild>
-                                        <Link
-                                            href={"#contact"}
-                                            className="text-lg"
-                                        >
-                                            Contact
-                                        </Link>
+                                        <Link href={"#contact"}>Contact</Link>
                                     </TooltipTrigger>
                                     <TooltipContent>
                                         <p>How to reach me</p>
