a144f022037b5edae902b3129d221f40d3548679
leonden <info@leonhochwimmer.com>
2024-04-25 15:06:26 +0200

    Continue with preloader

--- components/preloader.tsx
+++ components/preloader.tsx
@@ -1,10 +1,33 @@
-import React from "react"
+"use client"
+import React, { useState } from "react"
 import { Progress } from "./ui/progress"
 
 export default function Preloader() {
+    const [progress, setProgress] = useState(0)
+
+    React.useEffect(() => {
+        const randomTime = Math.floor(Math.random() * 3000) + 1000
+        const randomSteps = Math.floor(Math.random() * 3) + 1
+        let currentProgress = 0
+
+        const updateProgress = () => {
+            if (currentProgress < 100) {
+                const step = Math.floor(100 / randomSteps)
+                currentProgress = Math.min(currentProgress + step, 100)
+                setProgress(currentProgress)
+                setTimeout(updateProgress, randomTime / randomSteps)
+            }
+        }
+
+        setTimeout(updateProgress, randomTime)
+    }, [])
+
     return (
-        <div>
-            <Progress />
+        <div className="w-screen h-screen grid place-items-center z-50 bg-white">
+            <div className="w-2/3 md:w-1/2 xl:w-2/5 2xl:w-1/5">
+                <h3 className="pl-2 text-2xl mb-2">The Art of Creating.</h3>
+                <Progress value={progress} />
+            </div>
         </div>
     )
 }
