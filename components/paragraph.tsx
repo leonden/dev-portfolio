9d920abe3aaa88ff0ae8b8b46546a06189d93d65
Leon <leon.hochwimmer@breitband.ch>
2024-04-28 13:50:35 +0200

    Fix inview animation

--- components/paragraph.tsx
+++ components/paragraph.tsx
@@ -1,23 +1,27 @@
 "use client"
-import React from "react"
-import { motion } from "framer-motion"
+import React, { useRef } from "react"
+import { motion, useInView } from "framer-motion"
 
 type ParagraphProps = {
     children: React.ReactNode | string
-    className: string
+    className?: string
 }
 
 export default function Paragraph({ children, className }: ParagraphProps) {
+    const ref = useRef(null)
+    const inView = useInView(ref, { once: true })
+
     return (
         <motion.p
-            className={className}
+            className={className ? className : ""}
             initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
+            animate={inView ? { opacity: 1, scale: 1 } : {}}
             transition={{
-                duration: 1,
+                duration: 1.5,
                 delay: 0.5,
-                ease: [0, 0.71, 0.9, 1.01],
+                ease: [0, 0.71, 0.75, 1.01],
             }}
+            ref={ref}
         >
             {children}
         </motion.p>
